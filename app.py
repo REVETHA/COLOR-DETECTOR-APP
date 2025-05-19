edb950503aae5937399e873e416c923b8c10ec4e
REVETHA K <revethakumaran795@gmail.com>
2025-05-20 02:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def about():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) # get port form the environment
+    app.run(host="0.0.0.0", port=port) # Bind to 0.0.0.0
